768169dcea051dabc5ed22db035f8b33dbc2ae74
Ram kiran Randhi <randhiramkiran@gmail.com>
2025-08-04 22:59:18 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static String  PrintHello(){
+        String n="HelloWorld";
+        return n;
+    }
+    public static void main(String[] args) {
+        String Hello=PrintHello();
+        System.out.println(Hello);
+        
+    }
+}
